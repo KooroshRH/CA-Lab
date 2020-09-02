3f3157f71b03242a5c562da33ebb5805dec88c77
fatemeh_vlp <fatemeh.vpasha@gmail.com>
2020-09-02 12:04:26 +0430

    edits Question#2

--- Lab-3/Question#2/tff_test.vhd
+++ Lab-3/Question#2/tff_test.vhd
@@ -18,7 +18,7 @@ signal qo, cl, res, din : std_logic;
 begin
 tff : tff_async_reset port map (q => qo, clk => cl, reset => res, data => din);
 res <= '1', '0' after 300 ns;
-din <= '1';
+din <= '0', '1' after 200 ns;
 cl <= '0', '1' after 100 ns, '0' after 200 ns, '1' after 300 ns, '0' after 400 ns, '1' after 500 ns;
 
 end test;
