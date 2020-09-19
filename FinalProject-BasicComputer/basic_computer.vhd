b11271e8d7a5460d80e6ae600d5a3c53978ca624
Korosh <korosh.roohi9731@gmail.com>
2020-09-19 09:17:24 +0430

    Add basic computer vhd file

---
+++ FinalProject-BasicComputer/basic_computer.vhd
@@ -0,0 +1,14 @@
+library IEEE; 
+use IEEE.STD_LOGIC_1164.all;  
+
+entity basic_computer is 
+   port( 
+      	clk: in std_logic;
+	res: out std_logic_vector(15 downto 0)
+   ); 
+end basic_computer;  
+
+architecture behavioral of basic_computer is 
+begin 
+   
+end behavioral;
